c193340db0cc6299820fc403ab61e9dc6233036b
petfle <peter.tollnes.flem@knowit.no>
2014-11-28 10:12:40 +0100

    First commit. The examlpe is mostly complete. Could be polished, if wanted.

---
+++ MultipleFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using MultipleFileUpload.Models;
+using System.Linq;
+
+namespace MultipleFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        //
+        // POST: /Home/PostFiles
+
+        [HttpPost]
+        public JsonResult PostFiles(IEnumerable<PostedFile> files)
+        {
+            // Just returning some information
+            var fileInformation = files.Select(x => new {filename = x.File.FileName, title = x.Title});
+            return Json(new { submitedFiles = new JavaScriptSerializer().Serialize(fileInformation) });
+        }
+
+    }
+}
---
+++ MultipleFileUpload/Models/PostedFile.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+
+namespace MultipleFileUpload.Models
+{
+    public class PostedFile
+    {
+        public string Title { get; set; }
+        public HttpPostedFileBase File { get; set; }
+    }
+}
