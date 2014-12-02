b5193f6a3d75b2a28b15ff7235f507dc9b1f230c
petfle <peter.tollnes.flem@knowit.no>
2014-12-02 13:01:47 +0100

    Moving the logic for the handling of the upload inputs out into its own file, making it more generic.

--- MultipleFileUpload/Resources/javascript/main.js
+++ MultipleFileUpload/Resources/javascript/main.js
@@ -1,21 +1,26 @@
 ﻿(function ($) {
 
-    var validExtensions = ['txt', 'png', 'jpg'];        // All the valid file extensions
-    var fileUploadInputs = $('#file-upload-inputs');    // List element containing all the li elements with the input fields
     var errorMessageDiv = $('#error-messages');         // The div that contains the error message
     
+    window.MultipleFileUpload.create({
+        containerId: 'multiple-file-upload',
+        addMoreButtonId: 'add-new-button-template',
+        validExtensions: ['txt', 'png', 'jpg'],
+        rowDeleted: function () {
+            toggleErrorMessageForFileUpload();
+        },
+        invalidFileType: function (row) {
+            $(row).addClass('error-row');
+            toggleErrorMessageForFileUpload();
+        }
+    });
+    
     bindDomElementEvents(); // Bind necessary dom element events
-    addNewDocumentInput();  // Adds the initial first file upload li
 
     function bindDomElementEvents() {
-        $('#add-more').on('click', function() {
-            addNewDocumentInput();
-        });
-
         $('#form').ajaxForm({
             url: '/Home/PostFiles',
             type: 'POST',
-            beforeSerialize: handlePreSerialize,
             success: function (response) {
                 $('#feedback').html('Submited: ' + response.submitedFiles);
             },
@@ -25,46 +30,8 @@
         });
     }
     
-    function isFormValid() {
-        return !$('.row:first-child', fileUploadInputs.children()).is('.error-row');
-    }
-    
-    function handlePreSerialize() {
-        var files = hasFiles();
-        var formValid = isFormValid();
-        
-        if (!files)
-            $('#feedback').html('You have to add a file');
-
-        if (!files || !formValid) return false;
-        
-        mapFilesAndTitles();
-    }
-
-    function addNewDocumentInput() {
-        var li = getNewListElement();
-
-        attachNewListElementEventListeners(li);
-        fileUploadInputs.append(li);
-    }
-
-    function isValidFileType(filename) {
-        // If there are NO extensions, allow everything
-        if (!validExtensions.length)
-            return true;
-
-        var extension = filename.substring(filename.lastIndexOf('.') + 1);
-        if (!~validExtensions.indexOf(extension.toLowerCase()))
-            return false;
-
-        return true;
-    }
-
-    function hasFiles() {
-        // Check if any file input field contains a file
-        return $('input[type="file"]', fileUploadInputs.children()).is(function() {
-            return !!this.files.length;
-        });
+    function isAllFileUploadsValid() {
+        return !$('#multiple-file-upload').children().hasClass('error-row');
     }
 
     function createErrorMessageLabel() {
@@ -73,70 +40,10 @@
     }
 
     function toggleErrorMessageForFileUpload() {
-        if (isFormValid())
+        if (isAllFileUploadsValid())
             errorMessageDiv.empty();
         else if (errorMessageDiv.children().length === 0)
             errorMessageDiv.append(createErrorMessageLabel());
-    }
-
-    function mapFilesAndTitles() {
-        var listElements = $('li', fileUploadInputs);
-        // We can't use the $.each index, because there might be 
-        // empty input fields which we doesn't want to submit. We need
-        // a cohesive series of numbers in the name field.
-        var inputCounter = 0;
-        
-        $.each(listElements, function (i, li) {
-            var fileAndTitlePair = li.getElementsByTagName('input');
-            var fileInput = fileAndTitlePair[0];
-            var titleInput = fileAndTitlePair[1];
-            
-            if (!fileInput.files.length) {
-                $(fileInput).removeAttr('name');
-                $(titleInput).removeAttr('name');
-            } else {
-                $(fileInput).prop('name', 'files[' + inputCounter + '].File');
-                $(titleInput).prop('name', 'files[' + inputCounter + '].Title');
-                inputCounter++;
-            }
-        });
-    }
-    
-    function getNewListElement() {
-        return $('<li>\
-            <div class="row">\
-                <div class="col-xs-7 name">\
-                    <input type="file" class="relevantDocuments" title="Choose file" />\
-                </div>\
-                <div class="col-xs-3">\
-                    <input type="text" placeholder="Enter a title here" />\
-                </div>\
-                <div class="col-xs-2 delete-link">\
-                    <a href="javascript:void(0)">Delete</a>\
-                </div>\
-            </div>\
-        </li>')[0];
-    }
-
-    function attachNewListElementEventListeners(li) {
-        $('a', li).on('click', function () {
-            $(this).closest('li').remove();
-            toggleErrorMessageForFileUpload();
-        });
-
-        $('input[type="file"]', li).on('change', function () {
-            var file = this.files[0];
-            
-            if (!file)
-                return;
-
-            var closestRow = $(this).closest('.row');
-            isValidFileType(file.name)
-                ? closestRow.removeClass('error-row')
-                : closestRow.addClass('error-row');
-
-            toggleErrorMessageForFileUpload();
-        });
     }
 
 })(jQuery);
---
+++ MultipleFileUpload/Resources/javascript/multiple-file-upload.js
@@ -0,0 +1,118 @@
+﻿(function(_this) {
+
+    var options = {};
+    var defaultOptions = {
+        validExtensions: [],
+        containerId: '',
+        addMoreButtonId: 'add-more-button',
+        rowDeleted: function () { },
+        invalidFileType: function () { }
+    };
+    
+    function getNode(templateId) {
+        var div = document.createElement('div');
+        div.innerHTML = get(templateId).innerHTML;
+        return div.children[0];
+    }
+
+    function addNewFileInput() {
+        var row = getNode('file-upload-row');
+        var container = get(options.containerId);
+
+        row.className += ' file-upload-row';
+
+        container.appendChild(row);
+        attachNewListElementEventListeners(row);
+    }
+    
+    function attachNewListElementEventListeners(row) {
+        var _row = row;
+        row.getElementsByTagName('a')[0].addEventListener('click', function () {
+            _row.parentNode.removeChild(_row);
+            mapFilesAndTitles();
+            options.rowDeleted(); // callback on row deleted
+        });
+
+        // only css2 selectors in ie8
+        row.querySelector('input[type="file"]').addEventListener('change', function () {
+            var file = this.files[0];
+
+            if (!file)
+                return;
+
+            mapFilesAndTitles();
+            !isValidFileType(file.name) && options.invalidFileType(_row); // callback on invalid row
+        });
+    }
+    
+    function mapFilesAndTitles() {
+        // We can't use the $.each index, because there might be 
+        // empty input fields which we doesn't want to submit. We need
+        // a cohesive series of numbers in the name field.
+        var inputCounter = 0;
+        var rows = get(options.containerId).querySelectorAll('.file-upload-row'); // only css2 selectors in ie8
+
+        Array.prototype.slice.call(rows, 0).forEach(function (element) {
+            var fileAndTitlePair = element.getElementsByTagName('input');
+            var fileInput = fileAndTitlePair[0];
+            var titleInput = fileAndTitlePair[1];
+
+            if (!fileInput.files.length) {
+                fileInput.removeAttribute('name');
+                titleInput.removeAttribute('name');
+            } else {
+                fileInput.setAttribute('name', 'files[' + inputCounter + '].File');
+                titleInput.setAttribute('name', 'files[' + inputCounter + '].Title');
+                inputCounter++;
+            }
+        });
+    }
+    
+    function isValidFileType(filename) {
+        // If there are NO extensions, allow everything
+        if (!options.validExtensions.length)
+            return true;
+
+        var extension = filename.substring(filename.lastIndexOf('.') + 1);
+        if (!~options.validExtensions.indexOf(extension.toLowerCase()))
+            return false;
+
+        return true;
+    }
+    
+    function addBaseMarkup() {
+        var addNewRowButton = getNode('add-new-button-template');
+        addNewRowButton.addEventListener('click', function() {
+            addNewFileInput();
+        });
+        
+        insertAfter(addNewRowButton, get(options.containerId));
+    }
+
+    _this.create = function(passedInOptions) {
+        options = extend(defaultOptions, passedInOptions);
+
+        addBaseMarkup();
+        addNewFileInput();
+        
+        get(options.addMoreButtonId).addEventListener('click', function () {
+            addNewFileInput();
+        });
+    };
+
+    function extend(a, b) {
+        for (var attrname in b)
+            a[attrname] = b[attrname];
+    
+        return a;
+    }
+    
+    function get(id) {
+        return document.getElementById(id);
+    }
+    
+    function insertAfter(newNode, referenceNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
+
+})(window.MultipleFileUpload = window.MultipleFileUpload || {});
